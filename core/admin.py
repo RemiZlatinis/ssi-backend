ffa100f15dfda12b07d635e7f6d9dc74c8ca36f0
RemiZlatinis <remizlatinis@gmail.com>
2025-09-14 05:02:15 +0300

    Initialize the agent model

--- core/admin.py
+++ core/admin.py
@@ -1,1 +1,9 @@
-# Register your models here.
+from django.contrib import admin
+
+from .models import Agent
+
+
+@admin.register(Agent)
+class AgentAdmin(admin.ModelAdmin):
+    list_display = ("name", "owner", "created_at")
+    readonly_fields = ("key", "created_at")
--- core/models.py
+++ core/models.py
@@ -1,1 +1,18 @@
-# Create your models here.
+import uuid
+
+from django.contrib.auth import get_user_model
+from django.db import models
+
+User = get_user_model()
+
+
+class Agent(models.Model):
+    name = models.CharField(
+        max_length=50, help_text="Friendly name provided by the user"
+    )
+    key = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="agents")
+
+    def __str__(self):
+        return self.name
