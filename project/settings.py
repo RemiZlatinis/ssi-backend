6f09d06f5451799eb5b69f476e8d8925cfa2971a
RemiZlatinis <remizlatinis@gmail.com>
2025-09-14 09:48:26 +0300

    Initialize the usage of .env

--- project/settings.py
+++ project/settings.py
@@ -10,7 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
+
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,14 +25,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-sky3_e!dj3^^1qm01wk(d(u)$nu!3(m8gqc6x&%#8b3m6e^=i("
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    "192.168.1.*",
+    "127.0.0.1",
 ]
+
+host_env = os.getenv("HOST")
+if host_env:
+    ALLOWED_HOSTS.append(host_env)
 
 
 # Application definition
