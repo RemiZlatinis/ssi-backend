3597d1dece0ce591e8f6b832aadb37aa30090f56
RemiZlatinis <remizlatinis@gmail.com>
2025-09-20 16:47:45 +0300

    feat(containerization): Adding a management command for healthcheck

---
+++ core/management/commands/healthcheck.py
@@ -0,0 +1,17 @@
+from django.core.management.base import BaseCommand
+from django.db import connections
+from django.db.utils import OperationalError
+
+
+class Command(BaseCommand):
+    help = "Check if the database is available"
+
+    def handle(self, *args, **options):
+        db_conn = connections["default"]
+        try:
+            db_conn.cursor()
+        except OperationalError:
+            self.stdout.write(self.style.ERROR("Database unavailable"))
+            exit(1)
+        else:
+            self.stdout.write(self.style.SUCCESS("Database available"))
